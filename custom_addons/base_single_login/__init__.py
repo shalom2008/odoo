8954d60e3a9a31e2eec52468be6602616bbc8c6d
shalom2008 <shalom2008@users.noreply.github.com>
2018-10-24 16:02:02 +0800

    fix a little bug

--- custom_addons/base_single_login/__init__.py
+++ custom_addons/base_single_login/__init__.py
@@ -1,4 +1,2 @@
-# -*- coding: utf-8 -*-
-
 import res_users
 import constroller
--- custom_addons/base_single_login/res_users.py
+++ custom_addons/base_single_login/res_users.py
@@ -6,7 +6,6 @@
 from odoo import SUPERUSER_ID, models
 from odoo import tools,fields,models,api
 
-
 class res_users(models.Model):
     _inherit="res.users"
 
@@ -16,12 +15,12 @@ class res_users(models.Model):
 class BaseConfigSettings(models.TransientModel):
     _inherit = 'base.config.settings'
 
-    once_login = fields.Boolean(string='单次登陆', help='一个帐号只允许一次登录，'
-                                '当前帐号登录时，清除该帐号下其它客户的session',
-                                defaults=False)
+
+    once_login = fields.Boolean(string='单次登陆', help='一个帐号只允许一次登录，当前帐号登录时，清除该帐号下其它客户的session',defaults=False)
+
 
     @api.model
-    def get_default_once_login(self):
+    def get_default_once_login(self,fields):
         configs=self.env['base.config.settings'].search([])
         length = len(configs)
         if length != 0:
@@ -29,6 +28,7 @@ class BaseConfigSettings(models.TransientModel):
             return {"once_login": config.once_login}
         else:
             return {}
+
 
     @api.model
     def signin_login(self):
